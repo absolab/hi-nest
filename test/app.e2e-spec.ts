e96162a8739c9e7c0d2b0b926a46d173540e0354
hertz <kljj0650@gmail.com>
2023-10-13 17:57:09 +0900

    E2E 추가 작성

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -30,7 +37,7 @@ describe('AppController (e2e)', () => {
         .expect([]);
     });
 
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/champions')
         .send({
@@ -43,8 +50,40 @@ describe('AppController (e2e)', () => {
         })
         .expect(201);
     });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/champions')
+        .send({
+          name: '아칼리',
+          passive: '암살자의 표식',
+          q: '오연투척검',
+          w: '황혼의 장막',
+          e: '표창곡예',
+          r: '무결처형',
+          other: 'thing',
+        })
+        .expect(400);
+    });
     it('DELETE', () => {
       return request(app.getHttpServer()).delete('/champions').expect(404);
+    });
+  });
+
+  describe('/champions/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/champions/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/champions/999').expect(404);
+    });
+    it('PATCH', () => {
+      return request(app.getHttpServer())
+        .patch('/champions/1')
+        .send({ name: 'Updated Test' })
+        .expect(200);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/champions/1').expect(200);
     });
   });
 });
